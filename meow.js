0b158d61c6846750fa09433b316f9cf8e15d25ec
Raya <125909608+coldravioli@users.noreply.github.com>
2024-12-20 14:52:23 -0500

    create!

---
+++ meow.js
@@ -0,0 +1,10 @@
+function toggleDropdown() {
+    var menu = document.getElementById("dropdownMenu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none"; // Hide the menu if it's already visible
+    } else {
+        menu.style.display = "block"; // Show the menu if it's hidden
+    }
+}
+
+
